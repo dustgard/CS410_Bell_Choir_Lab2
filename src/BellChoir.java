bb952258d68a1ba82ce33854ca82f670ad792771
dustgard <dustin.j.gardner@gmail.com>
2024-02-24 00:31:34 -0700

    Set FileChooser to the Songs directory

--- src/BellChoir.java
+++ src/BellChoir.java
@@ -4,8 +4,7 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class BellChoir {
 
     public static void main(String[] args) {
-        String homeDir = System.getProperty("user.home");
-        JFileChooser song = new JFileChooser(homeDir + "/Desktop");
+        JFileChooser song = new JFileChooser("src/Songs");
         song.showOpenDialog(null);
         String songLocation = song.getSelectedFile().getAbsolutePath();
         SongNotes notesPassed = new SongNotes(songLocation);
