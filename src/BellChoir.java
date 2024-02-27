59c469633c6f5460d11946501bac791f60e25e56
dustgard <dustin.j.gardner@gmail.com>
2024-02-27 15:09:44 -0700

    Trying running ant

--- src/BellChoir.java
+++ src/BellChoir.java
@@ -7,10 +7,12 @@ import java.util.List;
 public class BellChoir {
 
     public static void main(String[] args) throws LineUnavailableException {
-        JFileChooser song = new JFileChooser("src/Songs");
-        song.showOpenDialog(null);
-        String songLocation = song.getSelectedFile().getAbsolutePath();
-        SongNotes notesPassed = new SongNotes(songLocation);
+
+//        JFileChooser song = new JFileChooser("src/Songs");
+//        song.showOpenDialog(null);
+//        String songLocation = song.getSelectedFile().getAbsolutePath();
+//        SongNotes notesPassed = new SongNotes(songLocation);
+        SongNotes notesPassed = new SongNotes(args[0]);
         notesPassed.readFile();
         List<String> notes = notesPassed.getMusicNotes();
         HashSet<String> uniqueNotes = notesPassed.getUniqueNotes();
