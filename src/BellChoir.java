f7f4fe6bfee714a4108612f67addae1d68a96220
dustgard <dustin.j.gardner@gmail.com>
2024-04-02 11:49:22 -0600

    Added JavaDoc to BellChoir

--- src/BellChoir.java
+++ src/BellChoir.java
@@ -3,6 +3,18 @@ import java.io.IOException;
 import java.util.HashSet;
 import java.util.Queue;
 
+/**
+ * BellChoir Lab
+ * The purpose of this lab was to teach that the program does not control the order in which threads run
+ * unless designed to do this, but by the OS.
+ * The BellChoir Lab is a multithreading program that plays a songs from a file.
+ * The file is passed to the program by using ANT from the command line.
+ * Once the file is passed in the program, then parse the file into a playable song after validation of the proper
+ * file format.
+ * The program simulates a conductor signaling each member that has been assigned a bell representing a note
+ * from the file, to play at the appropriate time and length according to the song.
+ */
+
 public class BellChoir {
 
     public static void main(String[] args) throws LineUnavailableException, IOException {
